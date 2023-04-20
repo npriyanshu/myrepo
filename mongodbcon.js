69c9f7f7124cd57538f817bc6deba43899b8ed5d
Priyanshu Negi <npriyanshu63@gmail.com>
2023-04-20 14:58:28 +0530

    bas bhai bas

--- mongodbcon.js
+++ mongodbcon.js
@@ -5,9 +5,12 @@ const client = new MongoClient(url)
 async function dbConnect(){
     let connection = await client.connect();
     let db = await connection.db('e-comm');
-    let collection = await db.collection('products')
-    let result = await collection.find().toArray();
-    console.log(result)
+    let collection = db.collection('products');
+    return collection;
+
+    // let result = await collection.find().toArray();
+    // console.log(result)
     
 }
+// dbConnect()
 module.exports=dbConnect;
